548b064a75d7ba8a302bb1e30d716b6383e80c16
Ichiri Imad <ichiri02imad@gmail.com>
2024-12-25 11:37:04 +0100

    fixing action.ts errors

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -63,6 +63,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
   `;
   } catch (error) {
+    console.log(`Error while crating invoice: ${error}`);
     // If a database error occurs, return a more specific error.
     return {
       message: "Database Error: Failed to Create Invoice.",
@@ -107,6 +108,7 @@ export async function updateInvoice(
       WHERE id = ${id}
     `;
   } catch (error) {
+    console.log(`Error while updating invoice: ${error}`);
     return {
       message: "Database Error: Failed to Update Invoice.",
     };
@@ -125,6 +127,7 @@ export async function deleteInvoice(id: string) {
 
     return { message: "Deleted Invoice." };
   } catch (error) {
+    console.log(`Error while deleting invoice: ${error}`);
     return {
       message: "Database Error: Failed to Delete Invoice.",
     };
