3bb81f7134d0504a1d7f5f95fdf05f150eea1848
Ichiri Imad <ichiri02imad@gmail.com>
2024-12-25 11:42:35 +0100

    fixing ui/invoices/button.tsx error

--- app/ui/invoices/buttons.tsx
+++ app/ui/invoices/buttons.tsx
@@ -26,7 +26,10 @@ export function UpdateInvoice({ id }: { id: string }) {
 }
 
 export function DeleteInvoice({ id }: { id: string }) {
-  const deleteInvoiceWithId = deleteInvoice.bind(null, id);
+  const deleteInvoiceWithId: (formData: FormData) => void = deleteInvoice.bind(
+    null,
+    id
+  );
 
   return (
     <form action={deleteInvoiceWithId}>
